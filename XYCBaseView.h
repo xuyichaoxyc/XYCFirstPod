19753a1d2bd5a9e7dccc54a6713af7aad3f82603
xuyichao_xyc <1817854647@qq.com>
2021-11-02 09:34:41 +0800

    my first commit

---
+++ XYCBaseView.h
@@ -0,0 +1,18 @@
+//
+//  XYCBaseView.h
+//  XYCFirstPodPj
+//
+//  Created by xuyichao on 2021/11/2.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XYCBaseView : UIView
+
+@property(nonatomic, strong, readonly) UIView *alphaView;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XYCBaseView.m
@@ -0,0 +1,20 @@
+//
+//  XYCBaseView.m
+//  XYCFirstPodPj
+//
+//  Created by xuyichao on 2021/11/2.
+//
+
+#import "XYCBaseView.h"
+
+@implementation XYCBaseView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
